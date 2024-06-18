02e7bf4140bf78b0936f4d8b47864349c6438222
Alisa Kuzyagina <khairutdinovaaalisa@gmail.com>
2024-06-19 00:03:45 +0300

    ask for a name

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-def greet():
-    print('Welcome to the Brain Games!')
+from brain_games import cli
 
 def main():
-    greet()
+    print('Welcome to the Brain Games!')
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
